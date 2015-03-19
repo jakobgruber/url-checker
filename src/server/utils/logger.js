aa51ec6f974ba66fb84e57537cc46a76259d75e3
Jakob Gruber <der@jakobgruber.at>
2015-03-19 14:38:19 +0100

    removed unnecessary code

--- src/server/utils/logger.js
+++ src/server/utils/logger.js
@@ -8,14 +8,6 @@ var logDir = './logs';
 //create the logs directory if it doesn't exists
 mkdirp.sync( path.join(__dirname, '../../../logs') );
 
-//check if log dir exist
-stats = fs.lstatSync(logDir);
-if (!stats.isDirectory()) {
-    fs.mkdirSync(logDir, function(err) {
-        if (err) throw err;
-    });
-}
-
 var logger = new (winston.Logger)({
     transports: [
         new (winston.transports.File)({ level: 'debug', filename: './logs/debug.log'}),
