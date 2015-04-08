1c1e0f34ca2d3a1e25904e3bcfe3ab2557f9fa6e
Jakob Gruber <der@jakobgruber.at>
2015-04-08 14:24:45 +0200

    added new rss-feed for uptime-testing

--- src/server/config/config.js
+++ src/server/config/config.js
@@ -4,6 +4,7 @@ module.exports = {
     'testPort': 3344,
 
     'rssFeedUrls': [
-        'http://www.autorevue.at/feed/app-latest'
+        'http://www.autorevue.at/feed/app-latest',
+        'http://autorevue.at/feed/testlinks'
     ]
 };
